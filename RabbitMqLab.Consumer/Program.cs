ab5d588322a32ed6190d5c041d3bd7017c98b11f
Luis Felipe Ramalho Oliveira <luis_felipero@hotmail.com>
2025-07-31 14:36:46 -0300

    new project lab RabbitMq

---
+++ RabbitMqLab.Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "minha-fila", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"📥 Mensagem recebida: {message}");
+};
+
+channel.BasicConsume(queue: "minha-fila", autoAck: true, consumer: consumer);
+
+Console.WriteLine("🕐 Aguardando mensagens. Pressione [Enter] para sair.");
+Console.ReadLine();
---
+++ RabbitMqLab.Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "minha-fila", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+Console.Write("Digite uma mensagem: ");
+string mensagem = Console.ReadLine() ?? "Mensagem vazia";
+
+var body = Encoding.UTF8.GetBytes(mensagem);
+
+channel.BasicPublish(exchange: "", routingKey: "minha-fila", basicProperties: null, body: body);
+
+Console.WriteLine($"✅ Mensagem enviada: {mensagem}");
